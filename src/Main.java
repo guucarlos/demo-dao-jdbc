0c1eed83719b0b911f7cc0fe1de2ce0e2a979043
Gustavo Araujo <guucarlos18@gmail.com>
2026-01-13 21:39:00 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
+        Seller seller = new Seller(21,"Gustavo", "guucarlos18@gmail.com", new Date(), 4000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller );
     }
 }
