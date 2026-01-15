31303fa89340f737b8e5707d4a924a14c3c6f92c
Gustavo Araujo <guucarlos18@gmail.com>
2026-01-14 23:13:19 -0300

    SellerDaoJDBC and Daofactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Main {
 
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"Gustavo", "guucarlos18@gmail.com", new Date(), 4000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(seller );
